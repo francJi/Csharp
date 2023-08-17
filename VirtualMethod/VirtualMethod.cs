20d7d1ca56d1168f3dde0e608c164a14b20150ff
francJi <minkyuji95@hanmail.net>
2023-08-17 10:44:59 +0900

    virtual Method 구현 예시

--- VirtualMethod/VirtualMethod.cs
+++ VirtualMethod/VirtualMethod.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Unit
+            List<Unit> list = new List<Unit>();
+            list.Add(new Marine());
+            list.Add(new Zergling());
+
+            foreach(Unit unit in list) 
+            {
+                unit.Move();
+            }
+        }
+    }
+    public class Unit
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("두발로 걷기");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Unit 공격");
+        }
+    }
+
+    public class Marine : Unit
+    {
+
+    }
+
+    public class Zergling : Unit
+    {
+        public override void Move()
+        {
+            Console.WriteLine("네발로 걷기");
         }
     }
 }
