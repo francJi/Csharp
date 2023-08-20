e98a272dcfba7795c5782c5384fce06e55c4d559
francJi <minkyuji95@hanmail.net>
2023-08-20 21:30:45 +0900

    memoPad..

---
+++ memoPad/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace memoPad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] gameScreen = new string[20, 10];
+
+            for (int row = 0; row < gameScreen.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameScreen.GetLength(1); col++)
+                {
+                    if (col == 0 || row == gameScreen.GetLength(0) - 1 || row == 0 || col == gameScreen.GetLength(1) - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        gameScreen[row, col] = "■";
+                        Console.Write(gameScreen[row, col]);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        gameScreen[row, col] = "  ";
+                        Console.Write(gameScreen[row, col]);
+                        Console.ResetColor();
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
+
