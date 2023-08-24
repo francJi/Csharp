32d481d7d93171c9230ac412b00ce536fda9049a
francJi <minkyuji95@hanmail.net>
2023-08-24 14:46:06 +0900

    interface 연습

---
+++ interface/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace interfaceEx;
+
+internal class Program
+{
+    interface IMovable
+    {
+        public void Move(int x, int y) { }
+    }
+
+    public class Player : IMovable
+    {
+        public void Move(int x, int y) { }
+    }
+
+    public class Enemy : IMovable
+    {
+        public void Move(int x, int y) { }
+    }
+
+    static void Main(string[] args)
+    {
+        IMovable movableObject1 = new Player();
+        IMovable movableObject2 = new Enemy();
+
+        movableObject1.Move(1, 2);
+        movableObject2.Move(3, 7);
+    }
+}
