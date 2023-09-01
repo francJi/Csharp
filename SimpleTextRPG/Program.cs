0a13aa21cf493297ef9304cfe86fe833ce34ac1c
francJi <minkyuji95@hanmail.net>
2023-09-01 17:56:21 +0900

    4강 과제 TextRPG

---
+++ SimpleTextRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleTextRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
